5eb5224bddbd3395d54033ca76a8ec0cce3a5d2f
adonicristiangerman <adoni.cristiang@gmail.com>
2026-05-14 22:38:15 -0400

    Added error page for meal

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>Unable to find the meal requested</p>
+    </main>
+  );
+}
